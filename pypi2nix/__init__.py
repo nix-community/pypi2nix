602fcf6fa96c04c855bf44b3f910fa8381755a98
Rok Garbas <rok@garbas.si>
2013-03-18 14:54:36 +0100

    adding option to be able to pin to certain versions

--- pypi2nix/__init__.py
+++ pypi2nix/__init__.py
@@ -1,7 +1,9 @@
-import os
+import copy
 import json
+import distlib.version
 import distlib.locators
 
+#distlib.locators.default_locator.scheme = '
 
 TEMPLATE = """
   %(nixname)s = buildPythonPackage rec {
@@ -46,9 +48,12 @@ class Pypi2Nix(object):
             pins = self.get_pins(pins)
         self.pins = pins
 
+        self.rev_deps = {}
         self.dist = None
         self.dists = {}
         for dist_name in dists:
+            if self.pins and dist_name in self.pins:
+                dist_name = "%s (== %s)" % (dist_name, self.pins[dist_name])
             dist = distlib.locators.locate(dist_name)
             if self.dist is None:
                 self.dist = dist
@@ -65,19 +70,46 @@ class Pypi2Nix(object):
             dist_pins[line[0]] = line[1].strip()
         return dist_pins
 
-    def get_nixname(self, dist):
-        name = dist.name.split(' ')[0]
+    def get_nixname(self, name):
+        name = name.split(' ')[0]
         name = name.replace('.', '_').replace('-', '_')
         return name.lower()
 
-    def process(self, dist):
-        nixname = self.get_nixname(dist)
+    def process_dist(self, dep_name_full, rev_deps):
+        dep_name = dep_name_full.split(' ')[0]
+        if self.pins and dep_name in self.pins:
+            dep_name = "%s (== %s)" % (dep_name, self.pins[dep_name])
+        else:
+            dep_name = dep_name_full
+
+        try:
+            dep_dist = distlib.locators.locate(dep_name)
+        except distlib.version.UnsupportedVersionError:
+            # default version scheme (adaptive) should also fallback to
+            # legacy version scheme, doing this manually
+            # needed for "pytz (==2012g)" requirement
+            scheme = distlib.locators.default_locator.scheme
+            distlib.locators.default_locator.scheme = 'legacy'
+            try:
+                dep_dist = distlib.locators.locate(dep_name)
+            finally:
+                distlib.locators.default_locator.scheme = scheme
+        dep_nixname = self.get_nixname(dep_dist.name)
+
+        if dep_nixname not in self.dists:
+            print dep_name
+            self.process(dep_dist)
+
+        if dep_dist.name not in self.ignores and dep_nixname not in rev_deps:
+            return dep_nixname
+
+    def process(self, dist, rev_deps=[]):
+        nixname = self.get_nixname(dist.name)
         if nixname in self.dists:
             return nixname
 
-        buildtime_deps = []
-        if dist.download_url.endswith('.zip'):
-            buildtime_deps.append('pkgs.unzip')
+        rev_deps.append(nixname)
+        copy_rev_deps = copy.deepcopy(rev_deps)
 
         self.dists[nixname] = {
             'nixname': nixname,
@@ -85,37 +117,40 @@ class Pypi2Nix(object):
             'version': dist.version,
             'download_url': dist.download_url,
             'md5sum': dist.md5_digest,
-            'buildtime_deps': ' '.join(buildtime_deps),
         }
 
+        buildtime_deps = []
+        if dist.download_url.endswith('.zip'):
+            buildtime_deps.append('pkgs.unzip')
+        for dep_name in dist.get_requirements('setup'):
+            dep_nixname = self.get_nixname(dep_name)
+            self.rev_deps.setdefault(dep_nixname, [])
+            self.rev_deps[dep_nixname].append(nixname)
+            dep_nixname = self.process_dist(dep_name, [])
+            if dep_nixname:
+                buildtime_deps.append(dep_nixname)
+
         deps = []
-        for dep_name_full in dist.get_requirements('install'):
-
-            dep_name = dep_name_full.split(' ')[0]
-            if self.pins and dep_name in self.pins:
-                dep_name = "%s (== %s)" % (dep_name, self.pins[dep_name])
-            else:
-                dep_name = dep_name_full
-
-            dep_dist = distlib.locators.locate(dep_name)
-            dep_nixname = self.get_nixname(dep_dist)
-            if dep_nixname in self.dists:
-                continue
-            print dep_name
-            self.process(dep_dist)
-            if dep_dist.name not in self.ignores:
+        for dep_name in dist.get_requirements('install'):
+            dep_nixname = self.get_nixname(dep_name)
+            self.rev_deps.setdefault(dep_nixname, [])
+            self.rev_deps[dep_nixname].append(nixname)
+            dep_nixname = self.process_dist(dep_name, copy_rev_deps)
+            if dep_nixname:
                 deps.append(dep_nixname)
 
-        self.dists[nixname]['deps'] = ' '.join(deps)
-
-        return nixname
+        self.dists[nixname]['deps'] = (' '.join(
+            deps)).replace('setuptools', 'pkgs.setuptools')
+        self.dists[nixname]['buildtime_deps'] = (' '.join(
+            buildtime_deps)).replace('setuptools', 'pkgs.setuptools')
 
     def __str__(self):
         distname = self.dist.name + self.dist.version.replace('.', '')
         distname = distname[0].lower() + distname[1:]
         distname += 'Packages'
         if self.extends:
-            self.dists.update(self.extends)
+            for name in self.extends:
+                self.dists[name].update(self.extends[name])
         return ALL_TEMPLATE % {
             'distname': distname,
             'expressions': ''.join([
