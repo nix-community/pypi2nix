a4b592eba76bd893795dfcaf5209a25ebe7ddea6
Rok Garbas <rok@garbas.si>
2013-07-28 14:22:32 +0200

    parsing buildout  results and fetching metadata from pypi

--- pypi2nix/buildout.py
+++ pypi2nix/buildout.py
@@ -34,7 +34,10 @@ stdenv.mkDerivation {
     %(versions)s
     EOF
     python bootstrap.py
-    bin/buildout
+    bin/buildout -q
+    sed -i -e 's@self.name = specification.project_name.lower()@self.name, self.project_name = specification.project_name.lower(), specification.project_name@g' /home/rok/.buildout/eggs/tl.eggdeps*/tl/eggdeps/graph.py
+    sed -i -e 's@name_string = "%%s %%s" %% (node.name, node.dist.version)@name_string = "%%s %%s" %% (node.project_name, node.dist.version)@g' %(eggsdir)s/tl.eggdeps*/tl/eggdeps/plaintext.py
+    sed -i -e 's@name_string = node.name@name_string = node.project_name@g' %(eggsdir)s/tl.eggdeps*/tl/eggdeps/plaintext.py
     bin/eggdeps -nt %(name)s > $out
   '';
 }
@@ -72,9 +75,80 @@ class Buildout(object):
             shlex.split('/run/current-system/sw/bin/nix-build'),
             cwd=buildout_dir, shell=True
         )
-        return {
-            self.name: open(output.strip()).read()
+        return parse_buildout_result(open(output.strip()).read())
+
+
+def parse_buildout_result(text):
+
+    tmp_result, parents, previous = {}, [None], None
+    for line in text.split('\n'):
+        stripped_line = line.strip()
+        stripped_line_split = stripped_line.split(' ')
+        if len(stripped_line) == 0:
+            continue
+
+        # get the level (starting with 0)
+        if (len(line) - len(stripped_line)) % 4 == 0:
+            level = (len(line) - len(stripped_line)) / 4
+        else:
+            tmp = parents[-1].strip().split()[0]
+            tmp_result.setdefault(tmp, {})
+            tmp_result[tmp].setdefault('extras', [])
+            tmp_result[tmp].setdefault('dependencies', [])
+            tmp_result[tmp]['extras'].append(line.strip()[1:-1])
+            continue
+
+        # previous level
+        if previous is None:
+            previous_level = 0
+        elif (len(previous) - len(previous.strip())) % 4 == 0:
+            previous_level = (len(previous) - len(previous.strip())) / 4
+
+        if len(parents) == 1:
+            parents.append(line)
+        if level > previous_level:
+            parents.append(previous)
+        if level < previous_level:
+            parents = parents[:-1*(previous_level - level)]
+
+        parent = parents[-1]
+        parent_stripped_split = parent.strip().split()
+        tmp_result.setdefault(parent_stripped_split[0], {})
+        tmp_result[parent_stripped_split[0]].setdefault('extras', [])
+        tmp_result[parent_stripped_split[0]].setdefault('dependencies', [])
+        tmp_result[parent_stripped_split[0]]['dependencies'].append(
+            stripped_line_split[0])
+
+        # collect data
+        if (len(line) - len(stripped_line)) % 4 == 0:
+            tmp_result.setdefault(stripped_line_split[0], {})
+            tmp_result[stripped_line_split[0]].setdefault('extras', [])
+            tmp_result[stripped_line_split[0]].setdefault('dependencies', [])
+            tmp_result[stripped_line_split[0]]['version'] = \
+                stripped_line_split[1]
+
+        previous = line
+        if len(parents) == 1:
+            parents.append(line)
+
+    result = {}
+    for name, data in tmp_result.items():
+        result['%s%s-%s' % (
+            name,
+            len(data['extras']) and '[%s]' % ','.join(data['extras']) or '',
+            data['version']
+        )] = {
+            'name': name,
+            'version': data['version'],
+            'dependencies': [
+                '%s%s-%s' % (
+                    i,
+                    len(tmp_result[i]['extras']) and '[%s]' % ','.join(
+                       self. tmp_result[i]['extras']) or '',
+                    tmp_result[i]['version'])
+                for i in data['dependencies']]
         }
+    return result
 
 
 def run_buildout(eggsdir, config):
--- pypi2nix/cli.py
+++ pypi2nix/cli.py
@@ -2,32 +2,47 @@ import sys
 import copy
 import json
 import tempfile
-import multiprocessing
+import xmlrpclib
 from pypi2nix.buildout import run_buildout
 
-TMPL_TOP = '''{ pkgs, stdenv, fetchurl, python, lowPrio, self }:
+TMPL_START = '''{ pkgs, stdenv, fetchurl, python, lowPrio, self }:
 
 let
   inherit (self) buildPythonPackage;
-
 in
 {'''
-TMPL_BOTTOM = '''
+TMPL_END = '''
+}'''
+TMPL_VERSION_START = '''
+} // pkgs.lib.optionalAttrs (python.majorVersion == "%s") {
+'''
+TMPL_VERSION_END = '''
 }
 '''
-TMPL_EXPR = '''
-  {
+TMPL_NIX_EXPR = '''
+  "%(name)s" = buildPythonPackage {
     name = "%(name)s";
-  }
+    src = fetchurl {
+        url = "%(url)s";
+        md5 = "%(md5)s";
+    };
+    propagatedBuildInputs = [ %(dependencies)s ];
+    meta = {
+      description = "%(description)s";
+      homepage = "%(homepage)s";
+      license = "%(license)s";
+    };
+  };
 '''
 
 DEFAULT_ENVIRONMENT = "py27"
 ENVIRONMENTS = {
     u"py26": "python26Full",
     u"py27": "python27Full",
-    u"py32": "python32",
-    u"py33": "python33",
-    u"pypy": "pythonPyPyFull",
+}
+LIB_VERSIONS = {
+    u"py26": "2.6",
+    u"py27": "2.7",
 }
 
 
@@ -58,18 +73,57 @@ def main():
                 temp_spec = copy.deepcopy(spec)
                 temp_spec['environment'] = environment
                 temp_spec['buildInputs'].append(ENVIRONMENTS[environment])
-                configs.append(temp_spec)
+                configs.append((LIB_VERSIONS[environment], temp_spec))
         else:
             spec['buildInputs'].append(ENVIRONMENTS[DEFAULT_ENVIRONMENT])
             spec['environment'] = DEFAULT_ENVIRONMENT
-            configs.append(spec)
+            configs.append((LIB_VERSIONS[DEFAULT_ENVIRONMENT], spec))
 
-    results = []
-    #pool = multiprocessing.Pool(processes=4)
-    for config in configs:
-        #results.append(pool.apply_async(run_buildout, [eggsdir, config]))
-        results.append(run_buildout(eggsdir, config))
+    packages = {}
+    packages_per_version = {}
+    for lib_version, config in configs:
+        tmp = run_buildout(eggsdir, config)
+        packages_per_version.setdefault(lib_version, set([]))
+        packages_per_version[lib_version] = \
+            packages_per_version[lib_version].union(
+                set([i for i in tmp.keys()]))
+        packages.update(tmp)
 
-    for result in results:
-        print '-' * 80
-        print result
+    client = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
+    for package_name in packages:
+        metadata = client.release_data(
+            packages[package_name]['name'],
+            packages[package_name]['version'])
+        packages[package_name]['description'] = metadata['summary']
+        packages[package_name]['homepage'] = metadata['home_page']
+        packages[package_name]['license'] = metadata['license']
+
+        release = None
+        releases = client.release_urls(
+            packages[package_name]['name'],
+            packages[package_name]['version'])
+        for item in releases:
+            if item['packagetype'] == 'sdist' and \
+               item['python_version'] == 'source':
+                release = item
+                break
+        if release is None:
+            raise("No source/sdist release version for %s!" % package_name)
+        packages[package_name]['md5'] = release['md5_digest']
+        packages[package_name]['url'] = release['url']
+
+    print TMPL_START
+    for version in packages_per_version:
+        print TMPL_VERSION_START % version
+        for package in packages_per_version[version]:
+            tmp = copy.deepcopy(packages[package])
+            tmp['name'] = package
+            tmp['dependencies'] = ' '.join(['self."%s"' % i
+                                            for i in tmp['dependencies']
+                                            if i != package])
+            tmp.setdefault('buildInputs', '')
+            if tmp['url'].endswith('.zip'):
+                tmp['buildInputs'] = 'pkgs.unzip'
+
+            print TMPL_NIX_EXPR % tmp
+        print TMPL_VERSION_END
