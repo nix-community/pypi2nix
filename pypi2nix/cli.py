cb1de51b605736a25f69b73d4338ac6efc4ef82e
jpic <jamespic@gmail.com>
2015-04-05 02:22:17 +0200

    Replaced print with secho and colors ...

--- pypi2nix/cli.py
+++ pypi2nix/cli.py
@@ -24,19 +24,29 @@ def main(input_file, nix_path):
             '<input_file> was not correct type. check help for more info.')
 
     if py_file:
+        click.secho('Converting setup.py to requirements.txt', fg='yellow')
         txt_file = py2txt.do(py_file)
+        click.secho('Got %s' % txt_file, fg='green')
 
     if cfg_file:
-        json_file = cfg2txt.do(cfg_file)
+        click.secho('Converting %s to requirements.txt' % cfg_file, fg='yellow')
+        txt_file = cfg2txt.do(cfg_file)
+        click.secho('Got %s' % txt_file, fg='green')
 
     if txt_file:
+        click.secho('Converting %s to json' % txt_file, fg='yellow')
         json_file = txt2json.do(txt_file)
+        click.secho('Got %s' % json_file, fg='green')
 
     if json_file:
+        click.secho('Converting %s to wheels' % json_file, fg='yellow')
         wheels_file = json2wheels.do(json_file, nix_path=nix_path)
+        click.secho('Got %s' % wheels_file, fg='green')
 
     if wheels_file:
+        click.secho('Converting %s to nix' % wheels_file, fg='yellow')
         nix_file = wheels2nix.do(wheels_file, nix_path=nix_path)
+        click.secho('Got %s' % nix_file, fg='green')
 
     #defaultNix.do()
     #print '-> default.nix'
