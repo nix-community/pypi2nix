b6266997b2bc42f2602bfede7dbf0331f41b75a8
Daniel Hahler <git@thequod.de>
2015-04-05 04:26:06 +0200

    Move things back into src/

--- src/pypi2nix/cli.py
+++ src/pypi2nix/cli.py
@@ -1,9 +1,8 @@
-import sys
 import click
-import py2txt
-import txt2json
-import json2wheels
-import wheels2nix
+import pypi2nix.json2wheels
+import pypi2nix.py2txt
+import pypi2nix.txt2json
+import pypi2nix.wheels2nix
 
 
 @click.command()
@@ -25,7 +24,7 @@ def main(input_file, nix_path):
 
     if py_file:
         click.secho('Converting setup.py to requirements.txt', fg='yellow')
-        txt_file = py2txt.do(py_file)
+        txt_file = pypi2nix.py2txt.do(py_file)
         click.secho('Got %s' % txt_file, fg='green')
 
     if cfg_file:
@@ -35,17 +34,17 @@ def main(input_file, nix_path):
 
     if txt_file:
         click.secho('Converting %s to json' % txt_file, fg='yellow')
-        json_file = txt2json.do(txt_file)
+        json_file = pypi2nix.txt2json.do(txt_file)
         click.secho('Got %s' % json_file, fg='green')
 
     if json_file:
         click.secho('Converting %s to wheels' % json_file, fg='yellow')
-        wheels_file = json2wheels.do(json_file, nix_path=nix_path)
+        wheels_file = pypi2nix.json2wheels.do(json_file, nix_path=nix_path)
         click.secho('Got %s' % wheels_file, fg='green')
 
     if wheels_file:
         click.secho('Converting %s to nix' % wheels_file, fg='yellow')
-        nix_file = wheels2nix.do(wheels_file, nix_path=nix_path)
+        nix_file = pypi2nix.wheels2nix.do(wheels_file, nix_path=nix_path)
         click.secho('Got %s' % nix_file, fg='green')
 
     #defaultNix.do()
