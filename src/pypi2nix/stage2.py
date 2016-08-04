878073380eca5eba1340fd93854e7767a4d60bb9
Rok Garbas <rok@garbas.si>
2016-08-04 23:00:46 +0200

    trying to match license from classifiers, fixes #66

--- src/pypi2nix/stage2.py
+++ src/pypi2nix/stage2.py
@@ -57,34 +57,125 @@ def extract_deps(metadata):
     return list(set(deps))
 
 
+all_classifiers = {
+    'License :: Aladdin Free Public License (AFPL)': None,
+    'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication': None,
+    'License :: DFSG approved': None,
+    'License :: Eiffel Forum License (EFL)': None,
+    'License :: Free For Educational Use': None,
+    'License :: Free For Home Use': None,
+    'License :: Free for non-commercial use': None,
+    'License :: Freely Distributable': None,
+    'License :: Free To Use But Restricted': None,
+    'License :: Freeware': None,
+    'License :: Netscape Public License (NPL)': None,
+    'License :: Nokia Open Source License (NOKOS)': None,
+    'License :: OSI Approved': None,
+    'License :: OSI Approved :: Academic Free License (AFL)': 'licenses.afl21',
+    'License :: OSI Approved :: Apache Software License': None,
+    'License :: OSI Approved :: Apple Public Source License': None,
+    'License :: OSI Approved :: Artistic License': 'licenses.artistic2',
+    'License :: OSI Approved :: Attribution Assurance License': None,
+    'License :: OSI Approved :: BSD License': 'licenses.bsdOriginal',
+    'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)': None,
+    'License :: OSI Approved :: Common Public License': 'licenses.cpl10',
+    'License :: OSI Approved :: Eiffel Forum License': 'licenses.efl20',
+    'License :: OSI Approved :: European Union Public Licence 1.0 (EUPL 1.0)': None,
+    'License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)': None,
+    'License :: OSI Approved :: GNU Affero General Public License v3': 'licenses.agpl3',
+    'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)': 'licenses.agpl3Plus',
+    'License :: OSI Approved :: GNU Free Documentation License (FDL)': 'licenses.fdl13',
+    'License :: OSI Approved :: GNU General Public License (GPL)': 'licenses.gpl1',
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)': 'licenses.gpl2',
+    'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)': 'licenses.gpl2Plus',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)': 'licenses.gpl3',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)': 'licenses.gpl3Plus',
+    'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)': 'licenses.lgpk2',
+    'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)': 'licenses.lgpl2Plus',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)': 'licenses.lgpl3',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)': 'licenses.lgpl3Plus',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)': 'licenses.lgpl2',
+    'License :: OSI Approved :: IBM Public License': 'licenses.ipl10',
+    'License :: OSI Approved :: Intel Open Source License': None,
+    'License :: OSI Approved :: ISC License (ISCL)': 'licenses.isc',
+    'License :: OSI Approved :: Jabber Open Source License': None,
+    'License :: OSI Approved :: MIT License': 'licenses.mit',
+    'License :: OSI Approved :: MITRE Collaborative Virtual Workspace License (CVW)': None,
+    'License :: OSI Approved :: Motosoto License': None,
+    'License :: OSI Approved :: Mozilla Public License 1.0 (MPL)': 'licenses.mpl10',
+    'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)': 'licenses.mpl11',
+    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)': 'licenses.mpl20',
+    'License :: OSI Approved :: Nethack General Public License': None,
+    'License :: OSI Approved :: Nokia Open Source License': None,
+    'License :: OSI Approved :: Open Group Test Suite License': None,
+    'License :: OSI Approved :: Python License (CNRI Python License)': None,
+    'License :: OSI Approved :: Python Software Foundation License': 'licenses.psfl',
+    'License :: OSI Approved :: Qt Public License (QPL)': None,
+    'License :: OSI Approved :: Ricoh Source Code Public License': None,
+    'License :: OSI Approved :: Sleepycat License': 'licenses.sleepycat',
+    'License :: OSI Approved :: Sun Industry Standards Source License (SISSL)': None,
+    'License :: OSI Approved :: Sun Public License': None,
+    'License :: OSI Approved :: University of Illinois/NCSA Open Source License': 'licenses.ncsa',
+    'License :: OSI Approved :: Vovida Software License 1.0': 'licenses.vsl10',
+    'License :: OSI Approved :: W3C License': 'licenses.w3c',
+    'License :: OSI Approved :: X.Net License': None,
+    'License :: OSI Approved :: zlib/libpng License': 'licenses.zlib',
+    'License :: OSI Approved :: Zope Public License': 'licenses.zpt21',
+    'License :: Other/Proprietary License': None,
+    'License :: Public Domain': 'publicDomain',
+    'License :: Repoze Public License': None,
+}
+
+
 def find_license(item):
-    license = item.get('license', '')
-    if license in ['LGPL with exceptions or ZPL', 'ZPL 2.1']:
-        license = "licenses.zpt21"
-    elif license in ['MIT', 'MIT License',
-                     'MIT or Apache License, Version 2.0']:
-        license = "licenses.mit"
-    elif license in ['BSD', 'BSD License', 'BSD-like',
-                     'BSD or Apache License, Version 2.0'] or \
-            license.startswith('BSD -'):
-        license = "licenses.bsdOriginal"
-    elif license in ['Apache 2.0', 'Apache License 2.0', 'Apache 2',
-                     'Apache License, Version 2.0']:
-        license = "licenses.asl20"
-    elif license in ['GNU Lesser General Public License (LGPL), Version 3',
-                     'LGPL']:
-        license = "licenses.lgpl3"
-    elif license in ['MPL 2.0', 'MPL 2.0 (Mozilla Public License)']:
-        license = "licenses.mpl20"
-    elif license in ['Python Software Foundation License']:
-        license = "licenses.psfl"
-    elif license is None:
-        license = '""'
-    else:
-        click.echo(
-            "WARNING: Couldn't recognize license `{}` for `{}`".format(
-                license, item.get('name')))
-        license = '"' + safe(license) + '"'
+    license = None
+
+    classifiers = item.get('classifiers', [])
+
+    # find first license classifier
+    all_classifiers_keys = all_classifiers.keys()
+    license_classifiers = [i for i in filter(
+        lambda x: x in all_classifiers_keys,
+        classifiers
+    )]
+    for license_classifier in license_classifiers:
+        license_nix = all_classifiers[license_classifier]
+        if license_nix is not None:
+            license = license_nix
+            break
+
+    if license is None:
+        license = item.get('license', '')
+
+        if license in ['LGPL with exceptions or ZPL', 'ZPL 2.1']:
+            license = "licenses.zpt21"
+        elif license in ['MIT', 'MIT License',
+                         'MIT or Apache License, Version 2.0']:
+            license = "licenses.mit"
+        elif license in ['BSD', 'BSD License', 'BSD-like',
+                         'BSD or Apache License, Version 2.0'] or \
+                license.startswith('BSD -'):
+            license = "licenses.bsdOriginal"
+        elif license in ['Apache 2.0', 'Apache License 2.0', 'Apache 2',
+                         'Apache License, Version 2.0']:
+            license = "licenses.asl20"
+        elif license in ['GNU Lesser General Public License (LGPL), Version 3',
+                         'LGPL']:
+            license = "licenses.lgpl3"
+        elif license in ['MPL 2.0', 'MPL 2.0 (Mozilla Public License)']:
+            license = "licenses.mpl20"
+        elif license in ['Python Software Foundation License']:
+            license = "licenses.psfl"
+
+        else:
+            if len(license_classifiers) > 0:
+                license = license_classifiers[0]
+            else:
+                click.echo(
+                    "WARNING: Couldn't recognize license `{}` for `{}`".format(
+                        license, item.get('name')))
+
+            license = '"{}"' .format(safe(license))
 
     return license
 
