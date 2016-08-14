7cee434aefba9057eef7f71d24ed6749a6ceddbb
Rok Garbas <rok@garbas.si>
2016-08-14 17:56:13 +0200

    adding sentry back to examples

--- examples/sentry_override.nix
+++ examples/sentry_override.nix
@@ -39,53 +39,5 @@ self: super: {
 #    buildInputs = [ pkgs.pyrex pkgs.libyaml ];
 #  });
 #
-  "sentry" = python.overrideDerivation super."sentry" (old: {
-    propagatedBuildInputs = old.propagatedBuildInputs ++ [ self."Pillow" ];
-  });
-#
-#
-#  "setuptools_scm" = python.mkDerivation rec {
-#    name = "setuptools_scm-1.7.0";
-#    src = pkgs.fetchurl {
-#      url = "https://pypi.python.org/packages/source/s/setuptools_scm/${name}.tar.gz";
-#      md5 = "d0423feeabda9c6a869d963cdc397d64";
-#    };
-#    buildInputs = [ self."pip" ];
-#    preBuild = ''
-#      ${python.interpreter.interpreter} setup.py egg_info
-#    '';
-#  };
-#
-#  "pip" = python.mkDerivation rec {
-#    name = "pip-8.0.2";
-#    src = pkgs.fetchurl {
-#      url = "http://pypi.python.org/packages/source/p/pip/${name}.tar.gz";
-#      md5 = "3a73c4188f8dbad6a1e6f6d44d117eeb";
-#    };
-#    doCheck = false;
-#    # pip detects that we already have bootstrapped_pip "installed", so we need
-#    # to force it a little.
-#    installFlags = [ "--ignore-installed" ];
-#  };
 
-  "Pillow" = python.mkDerivation rec {
-    name = "Pillow-3.2.0";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/P/Pillow/${name}.zip";
-      sha256 = "1rddmdg8vzjccfa6ri8l9ja166sqlyjlih0b7ngca9rb8d5wai6c";
-    };
-    doCheck = false;
-    buildInputs = [ pkgs.freetype pkgs.libjpeg pkgs.zlib pkgs.libtiff pkgs.libwebp pkgs.tcl pkgs.lcms2 ];
-    preConfigure = let
-      libinclude = pkg: ''"${pkg.out}/lib", "${pkg.dev}/include"'';
-    in ''
-      sed -i "setup.py" \
-          -e 's|^FREETYPE_ROOT =.*$|FREETYPE_ROOT = ${libinclude pkgs.freetype}|g ;
-              s|^JPEG_ROOT =.*$|JPEG_ROOT = ${libinclude pkgs.libjpeg}|g ;
-              s|^ZLIB_ROOT =.*$|ZLIB_ROOT = ${libinclude pkgs.zlib}|g ;
-              s|^LCMS_ROOT =.*$|LCMS_ROOT = _lib_include("${pkgs.libwebp}")|g ;
-              s|^TIFF_ROOT =.*$|TIFF_ROOT = ${libinclude pkgs.libtiff}|g ;
-              s|^TCL_ROOT=.*$|TCL_ROOT = _lib_include("${pkgs.tcl}")|g ;'
-    '';
-  };
 }
