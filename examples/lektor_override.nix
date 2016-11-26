af185cd5474a6c396133dcbfad16b2738c61cb19
Rok Garbas <rok@garbas.si>
2016-11-26 05:11:48 +0100

    fixing watchdog for lektor on darwin

--- examples/lektor_override.nix
+++ examples/lektor_override.nix
@@ -7,4 +7,11 @@ self: super: {
           ++ pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
    });
 
+   "watchdog" = python.overrideDerivation super."watchdog" (old: {
+      buildInputs = old.buildInputs
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin 
+              [ pkgs.darwin.apple_sdk.frameworks.CoreServices pkgs.darwin.cf-private ];
+ 
+   });
+
 }
