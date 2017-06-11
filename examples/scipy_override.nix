a85eb0ae4eee9a642b0668739713b0f0a43d7ec0
Rok Garbas <rok@garbas.si>
2017-06-11 03:11:24 +0200

    making scipy example build

--- examples/scipy_override.nix
+++ examples/scipy_override.nix
@@ -5,40 +5,53 @@ let
   lib = pkgs.lib;
 in
 
-self: super:
-let
-  is_numpy_dep = drv:
-    lib.hasSuffix
-    ("numpy-" + (builtins.parseDrvName drv).version)
-    (builtins.parseDrvName drv).name ;
-in {
-
-   "scipy" = python.overrideDerivation super."scipy" (old: {
-      buildInputs =
-        ( builtins.filter
-          (drv: ! (is_numpy_dep drv))
-          old.buildInputs
-        )++ [ pkgs.gfortran ];
-      propagatedBuildInputs = old.propagatedBuildInputs ++ [ blas self."numpy" ];
-      preBuild = ''
-        cat << EOF > site.cfg
-        [openblas]
-        include_dirs = ${blas}/include
-        library_dirs = ${blas}/lib
-        EOF
-      '';
-   });
+self: super: {
 
    "numpy" = python.overrideDerivation super."numpy" (old: {
-      buildInputs = old.buildInputs ++ [ pkgs.gfortran ];
-      propagatedBuildInputs = old.propagatedBuildInputs ++ [ blas ];
+      #patches = lib.optionals (python.hasDistutilsCxxPatch or false) [
+      #  # See cpython 2.7 patches.
+      #  # numpy.distutils is used by cython during it's check phase
+      #  ./numpy-distutils-C++.patch
+      #];
+      preConfigure = ''
+        sed -i 's/-faltivec//' numpy/distutils/system_info.py
+      '';
       preBuild = ''
+        echo "Creating site.cfg file..."
         cat << EOF > site.cfg
         [openblas]
         include_dirs = ${blas}/include
         library_dirs = ${blas}/lib
         EOF
       '';
+      postInstall = ''
+        ln -s $out/bin/f2py* $out/bin/f2py
+      '';
+      passthru = {
+        blas = blas;
+      };
+   });
+
+   "scipy" = python.overrideDerivation super."scipy" (old: {
+      prePatch = ''
+        rm scipy/linalg/tests/test_lapack.py
+      '';
+      preConfigure = ''
+        sed -i '0,/from numpy.distutils.core/s//import setuptools;from numpy.distutils.core/' setup.py
+      '';
+      preBuild = ''
+        echo "Creating site.cfg file..."
+        cat << EOF > site.cfg
+        [openblas]
+        include_dirs = ${blas}/include
+        library_dirs = ${blas}/lib
+        EOF
+      '';
+      setupPyBuildFlags = [ "--fcompiler='gnu95'" ];
+      passthru = {
+        blas = blas;
+      };
+
    });
 
 }
