3167baa8c9bfe823d48dbe8c65d33c1a7c8d8ac2
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-10-24 15:45:59 +0200

    Fix lektor travis build

--- examples/lektor_override.nix
+++ examples/lektor_override.nix
@@ -1,17 +1,36 @@
 { pkgs, python }:
+with pkgs.lib;
+let
+  specificOverrides =
+    self: super: {
 
-self: super: {
+       "cryptography" = python.overrideDerivation super."cryptography" (old: {
+          buildInputs = old.buildInputs
+              ++ optional
+                 pkgs.stdenv.isDarwin
+                 pkgs.darwin.apple_sdk.frameworks.Security;
+       });
 
-   "cryptography" = python.overrideDerivation super."cryptography" (old: {
-      buildInputs = old.buildInputs
-          ++ pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.darwin.apple_sdk.frameworks.Security;
-   });
+       "watchdog" = python.overrideDerivation super."watchdog" (old: {
+          buildInputs = old.buildInputs
+              ++ optionals pkgs.stdenv.isDarwin
+                  [ pkgs.darwin.apple_sdk.frameworks.CoreServices
+                    pkgs.darwin.cf-private
+                  ];
 
-   "watchdog" = python.overrideDerivation super."watchdog" (old: {
-      buildInputs = old.buildInputs
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin 
-              [ pkgs.darwin.apple_sdk.frameworks.CoreServices pkgs.darwin.cf-private ];
- 
-   });
-
-}
+       });
+    };
+  commonOverrides = self: super:
+    mapAttrs
+    (name: drv: python.overrideDerivation super."${name}" (old: {
+      postInstall =
+        ( if builtins.hasAttr "postInstall" old
+          then old.postInstall
+          else ""
+        ) + ''
+          rm -f $out/bin/*.pyc # hey emacs */
+        '';
+    }))
+    super;
+in
+composeExtensions commonOverrides specificOverrides
