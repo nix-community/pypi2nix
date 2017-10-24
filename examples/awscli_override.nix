40d86b8cd8a106aec87945cc2d5b3b29ae15683c
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-10-24 15:14:07 +0200

    Fix awscli package set

--- examples/awscli_override.nix
+++ examples/awscli_override.nix
@@ -1,10 +1,31 @@
 { pkgs, python }:
-
-self: super: {
-  "awscli" = python.overrideDerivation super."awscli" (old: {
-    propagatedBuildInputs = old.propagatedBuildInputs ++ [
-      pkgs.groff
-      pkgs.less
-    ];
-  });
-}
+with pkgs.lib;
+let
+  specificOverrides = self: super:
+  {
+    "awscli" = python.overrideDerivation super."awscli" (old: {
+      propagatedBuildInputs = old.propagatedBuildInputs ++ [
+        pkgs.groff
+        pkgs.less
+      ];
+    });
+    "jmespath" = python.overrideDerivation super."jmespath" (old: {
+      postInstall = ''
+        chmod +x $out/bin/jp.pyc
+      '';
+    });
+  };
+  commonOverrides = self: super:
+    mapAttrs
+    (name: drv: python.overrideDerivation super."${name}" (old: {
+      postInstall =
+        ( if builtins.hasAttr "postInstall" old
+          then old.postInstall
+          else ""
+        ) + ''
+          rm -f $out/bin/*.pyc # hey emacs */
+        '';
+    }))
+    super;
+in
+composeExtensions commonOverrides specificOverrides
