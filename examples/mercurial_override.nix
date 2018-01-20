49e18a50f98f2f29371c4f4e0829e1b348d6fe72
Rok Garbas <rok@garbas.si>
2018-01-20 03:56:43 +0100

    fixing examples and using --default-overrides flag

--- examples/mercurial_override.nix
+++ examples/mercurial_override.nix
@@ -2,15 +2,4 @@
 
 self: super: {
 
-   "pyflakes" = python.overrideDerivation super."pyflakes" (old: {
-      buildInputs = [ self."pytest-runner" ];
-   });
-
-   "mccabe" = python.overrideDerivation super."mccabe" (old: {
-      buildInputs = [ self."pytest-runner" ];
-   });
-
-   "pytest-runner" = python.overrideDerivation super."pytest-runner" (old: {
-      buildInputs = [ self."setuptools-scm" ];
-   });
 }
---
+++ examples/vulnix_override.nix
@@ -0,0 +1,16 @@
+{ pkgs, python }:
+
+self: super: {
+
+  "pytest-runner" = python.overrideDerivation super."pytest-runner" (old: {
+    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
+  });
+
+  "BTrees" = python.overrideDerivation super."BTrees" (old: {
+    propagatedNativeBuildInputs =
+      builtins.filter
+        (x: (builtins.parseDrvName x.name).name != "${python.__old.python.libPrefix}-${python.__old.python.libPrefix}-ZODB")
+        old.propagatedNativeBuildInputs;
+  });
+
+}
