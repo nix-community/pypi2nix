f30cffd2d40d8a66d75263b1865b5ca5914dbd49
Rok Garbas <rok@garbas.si>
2018-02-03 19:31:58 +0100

    fixing examples and using --default-overrides flag (#172)

--- examples/connexion_override.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, python }:
-
-self: super: {
-
-  "jsonschema" = python.overrideDerivation super."jsonschema" (old: {
-    propagatedBuildInputs = old.propagatedBuildInputs ++ [self.vcversioner];
-  });
-
-}
