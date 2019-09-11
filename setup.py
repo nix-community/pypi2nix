bc3db1b10b82688c3f5990ffb5694805a64b55e7
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-09-11 15:27:05 +0200

    Move all metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -2,41 +2,4 @@
 
 from setuptools import setup
 
-with open("src/pypi2nix/VERSION") as f:
-    version = f.read().strip()
-
-setup(
-    name="pypi2nix",
-    version=version,
-    author=u"Rok Garbas, Cillian de Róiste, Jaka Hudoklin",
-    description=(
-        "A tool that generates nix expressions for your python packages, so "
-        "you don't have to."
-    ),
-    license="BSD",
-    keywords="nixos nix packaging",
-    url="https://github.com/NixOS/pypi2nix",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-    ],
-    entry_points={"console_scripts": ["pypi2nix = pypi2nix.cli:main"]},
-    packages=["pypi2nix", "pypi2nix.pip"],
-    package_dir={
-        "": "src",
-        "pypi2nix.pip": "src/pypi2nix/pip",
-        "pypi2nix.wheels": "src/pypi2nix/wheels",
-    },
-    include_package_data=True,
-    install_requires=[
-        "attrs",
-        "click",
-        "jinja2",
-        "nix-prefetch-github",
-        "packaging",
-        "Parsley",
-        "requests",
-        "setupcfg",
-        "toml",
-    ],
-)
+setup()
