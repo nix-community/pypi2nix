e9733b4e7266639f5d54d85c105b6300a6577165
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-09-14 00:45:47 +0200

    Nuke custom environment marker implementation in favor of packaging.markers implementation

--- unittests/test_environment_marker.py
+++ unittests/test_environment_marker.py
@@ -1,12 +1,9 @@
 import pytest
 
 from pypi2nix.environment_marker import EnvironmentMarker
-from pypi2nix.environment_marker import MarkerToken
 
 
 @pytest.mark.parametrize("operator", ("<", "<=", "==", "!=", ">", ">="))
 def test_that_version_comparisons_do_not_throw(operator, current_platform):
-    marker = EnvironmentMarker(
-        operation=operator, left="1.0", right=MarkerToken.PYTHON_VERSION
-    )
+    marker = EnvironmentMarker(f"python_version {operator} '1.0'")
     marker.applies_to_platform(current_platform)
