949c92077fd330f998847809f76338523da84de9
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-09-24 20:32:57 +0200

    Remove unnecessary overrides in requirements_override.nix

--- requirements_override.nix
+++ requirements_override.nix
@@ -1,44 +1,23 @@
 { pkgs, python }:
 
-self: super: {
+self: super:
+let
+  addBuildInput = package: old: {
+    buildInputs = old.buildInputs ++ [ package ];
+  };
+in {
+  "fancycompleter" = python.overrideDerivation super."fancycompleter"
+    (addBuildInput self."setuptools-scm");
 
-  "fancycompleter" = python.overrideDerivation super."fancycompleter" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
+  "flake8-debugger" = python.overrideDerivation super."flake8-debugger"
+    (addBuildInput self."pytest-runner");
 
-  "flake8-debugger" = python.overrideDerivation super."flake8-debugger" (old: {
-    buildInputs = old.buildInputs ++ [ self."pytest-runner" ];
-  });
+  "mccabe" = python.overrideDerivation super."mccabe"
+    (addBuildInput self."pytest-runner");
 
-  "importlib-metadata" = python.overrideDerivation super."importlib-metadata" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-  "mccabe" = python.overrideDerivation super."mccabe" (old: {
-    buildInputs = old.buildInputs ++ [ self."pytest-runner" ];
-  });
+  "pdbpp" = python.overrideDerivation super."pdbpp"
+    (addBuildInput self."setuptools-scm");
 
-  "pdbpp" = python.overrideDerivation super."pdbpp" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-
-  "pluggy" = python.overrideDerivation super."pluggy" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-
-  "py" = python.overrideDerivation super."py" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-
-  "pytest" = python.overrideDerivation super."pytest" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-
-  "pytest-runner" = python.overrideDerivation super."pytest-runner" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-
-  "zipp" = python.overrideDerivation super."zipp" (old: {
-    buildInputs = old.buildInputs ++ [ self."setuptools-scm" ];
-  });
-
+  "py" = python.overrideDerivation super."py"
+    (addBuildInput self."setuptools-scm");
 }
