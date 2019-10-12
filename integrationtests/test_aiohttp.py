48a61ac64e5c1991444395a1f7cba3f021d72bc9
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-10-12 13:24:47 +0200

    Use python35 for aiohttp integrationtest

--- integrationtests/test_aiohttp.py
+++ integrationtests/test_aiohttp.py
@@ -5,3 +5,4 @@ class AiohttpTestCase(IntegrationTest):
     name_of_testcase = "aiohttp"
     code_for_testing = ["import aiohttp"]
     requirements = ["aiohttp==2.0.6.post1"]
+    python_version = "python35"
