9e8326abf77b8a62ed7c9051175fd0b1a82ebaf0
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-10-20 12:58:00 +0200

    Fix lektor integrationtest

--- integrationtests/ldap/requirements_override.nix
+++ integrationtests/ldap/requirements_override.nix
@@ -5,5 +5,4 @@ self: super: {
    "python-ldap" = python.overrideDerivation super."python-ldap" (old: {
       NIX_CFLAGS_COMPILE = "-I${pkgs.cyrus_sasl.dev}/include/sasl";
    });
-
 }
--- integrationtests/lektor/requirements_override.nix
+++ integrationtests/lektor/requirements_override.nix
@@ -1,10 +1,13 @@
 { pkgs, python }:
 
 self: super: {
-
   "lektor" = python.overrideDerivation super."lektor" (old: {
     patchPhase = ''
       sed -i -e "s|requests\[security\]|requests|" setup.py
     '';
   });
+
+   "pip" = super."pip".overrideDerivation (old: {
+     pipInstallFlags = ["--ignore-installed"];
+   });
 }
