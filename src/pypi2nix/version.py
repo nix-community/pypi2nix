c38223e1ccdd43bb0ce6fbe12664d048ee01efb0
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-11-10 16:43:34 +0100

    Fix integration tests by unsetting PYTHONPATH environment variable

--- src/pypi2nix/version.py
+++ src/pypi2nix/version.py
@@ -1,6 +1,6 @@
 try:
-    from ._version import pypi2nix_version
-except ImportError:
+    from ._version import pypi2nix_version  # type: ignore
+except ModuleNotFoundError:
     from setuptools_scm import get_version
 
     pypi2nix_version = get_version()
