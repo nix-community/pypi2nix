6d67028e293a7009d0ddec38595fb2f31bc52e0c
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-12-02 22:38:35 +0100

    Enable package index to handle git sources
    We use pip from master as a dependency in our build process as this fixes
    a bug that prevents flit from being built.

--- scripts/update_python_packages.py
+++ scripts/update_python_packages.py
@@ -4,22 +4,39 @@ import sys
 
 from pypi2nix.logger import StreamLogger
 from pypi2nix.pypi import Pypi
-from pypi2nix.wheels import INDEX
+from pypi2nix.utils import prefetch_git
+from pypi2nix.wheels import Index
 
 
 def main():
     logger = StreamLogger(sys.stdout)
     pypi = Pypi(logger=logger)
-    requirements = ["pip", "setuptools", "wheel"]
-    for name in requirements:
-        package = pypi.get_package(name)
-        source_release = pypi.get_source_release(name=name, version=package.version)
-        if source_release is None:
-            logger.warning(f"Could not update source for package `{name}`")
-            continue
-        INDEX[name] = INDEX.Entry(
-            url=source_release.url, sha256=source_release.sha256_digest
-        )
+    pip_requirements = ["setuptools", "wheel"]
+    git_requirements = [("pip", "https://github.com/pypa/pip.git")]
+    index = Index(logger=logger)
+    for name in pip_requirements:
+        insert_pip_requirement(index, name, logger, pypi)
+    for name, url in git_requirements:
+        insert_git_requirement(index, name, url)
+    assert index.is_valid()
+
+
+def insert_pip_requirement(index, requirement, logger, pypi):
+    package = pypi.get_package(requirement)
+    source_release = pypi.get_source_release(name=requirement, version=package.version)
+    if source_release is None:
+        logger.warning(f"Could not update source for package `{requirement}`")
+        return
+    index[requirement] = index.UrlEntry(
+        url=source_release.url, sha256=source_release.sha256_digest
+    )
+
+
+def insert_git_requirement(index, name, url):
+    repo_data = prefetch_git(url)
+    index[name] = index.GitEntry(
+        url=repo_data["url"], rev=repo_data["rev"], sha256=repo_data["sha256"],
+    )
 
 
 if __name__ == "__main__":
--- src/pypi2nix/pip/bootstrap.nix
+++ src/pypi2nix/pip/bootstrap.nix
@@ -1,9 +1,12 @@
-{ stdenv
-, python
-, fetchurl
-}:
+{ stdenv, python, fetchurl, fetchgit }:
 
 let
+  _fetchSource = jsonValue:
+    let cleanedJsonValue = builtins.removeAttrs jsonValue [ "__type__" ];
+    in (if jsonValue."__type__" == "fetchurl" then
+      fetchurl cleanedJsonValue
+    else
+      fetchgit cleanedJsonValue);
   wrappedPip = ''
     #!${stdenv.shell}
 
@@ -11,16 +14,14 @@ let
   '';
   indexJson = with builtins; fromJSON (readFile ../wheels/index.json);
   packageOverrides = self: super: {
-    pip = super.pip.overridePythonAttrs(old: {
-      src = fetchurl indexJson.pip;
-    });
-    setuptools = super.setuptools.overridePythonAttrs(old: {
-      src = fetchurl indexJson.setuptools;
-    });
-    wheel = super.wheel.overridePythonAttrs(old: {
-      src = fetchurl indexJson.wheel;
-    });
+    pip = super.pip.overridePythonAttrs
+      (old: { src = _fetchSource indexJson.pip; });
+    setuptools = super.setuptools.overridePythonAttrs
+      (old: { src = _fetchSource indexJson.setuptools; });
+    wheel = super.wheel.overridePythonAttrs
+      (old: { src = _fetchSource indexJson.wheel; });
   };
-  overriddenPython = python.override {inherit packageOverrides;};
-  pythonWithPackages = overriddenPython.withPackages(pkgs: with pkgs; [pip setuptools wheel]);
+  overriddenPython = python.override { inherit packageOverrides; };
+  pythonWithPackages =
+    overriddenPython.withPackages (pkgs: with pkgs; [ pip setuptools wheel ]);
 in pythonWithPackages
