fc364093c9ebd8ce2959f03925f13649d34ba640
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-01-21 20:54:32 +0100

    Add support for python38 (Python 3.8)

--- unittests/test_python_version.py
+++ unittests/test_python_version.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pypi2nix.python_version import PythonVersion
+from pypi2nix.python_version import python_version_from_version_string
 from pypi2nix.target_platform import PlatformGenerator
 
 from .switches import nix
@@ -13,3 +14,19 @@ def test_available_python_versions_exist_in_nixpkgs(
 ):
     target_platform = platform_generator.from_python_version(python_version)
     assert target_platform is not None
+
+
+@pytest.mark.parametrize(
+    "version_string, expected_python_version",
+    [
+        ("2.7", PythonVersion.python27),
+        ("3.5", PythonVersion.python35),
+        ("3.6", PythonVersion.python36),
+        ("3.7", PythonVersion.python37),
+        ("3.8", PythonVersion.python38),
+    ],
+)
+def test_can_get_python_version_from_version_string(
+    version_string, expected_python_version
+):
+    assert python_version_from_version_string(version_string) == expected_python_version
