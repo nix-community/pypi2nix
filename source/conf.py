36ad25c376d1993f8e26c9a557472bac9a68a3cd
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-03-08 14:11:33 +0100

    Add master_doc to sphinx conf

--- source/conf.py
+++ source/conf.py
@@ -32,6 +32,8 @@ author = 'Rok Garbas, Cillian de Róiste, Jaka Hudoklin'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
