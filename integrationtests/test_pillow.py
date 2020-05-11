c51ea7f23307e6ad3f7b623633ab10a661cac162
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-05-11 17:44:48 +0200

    Fix Pillow integration test on travis

--- integrationtests/test_pillow.py
+++ integrationtests/test_pillow.py
@@ -4,7 +4,7 @@ from .framework import IntegrationTest
 class PillowTestCase(IntegrationTest):
     name_of_testcase = "pillow"
     code_for_testing = ["import PIL"]
-    requirements = ["Pillow"]
+    requirements = ["Pillow==7.1.2"]
 
     external_dependencies = [
         "pkgconfig",
@@ -16,4 +16,5 @@ class PillowTestCase(IntegrationTest):
         "lcms2",
         "libwebp",
         "tcl",
+        "xorg.libxcb",
     ]
