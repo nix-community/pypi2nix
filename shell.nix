48ccdfdc58279080243193079c620dd4314cde35
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-05-27 15:10:03 +0200

    Use mkShell to provide development environment

--- shell.nix
+++ shell.nix
@@ -1,1 +1,16 @@
-import ./default.nix {}
+let
+  nixpkgs = import <nixpkgs> {};
+  pythonPackages = import ./requirements.nix { pkgs = nixpkgs; };
+in
+nixpkgs.mkShell {
+  buildInputs = with nixpkgs; [
+    pythonPackages.interpreter
+    nixfmt
+    git
+    nix-prefetch-hg
+  ];
+  shellHook = ''
+    export PATH=${./scripts}:$PATH
+    export PYTHONPATH=${./src}:$PYTHONPATH
+  '';
+}
