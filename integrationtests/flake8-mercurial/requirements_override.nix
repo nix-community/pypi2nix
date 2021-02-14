b22a87dfc81419af1e980be1f8ba955ee6959cf0
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-02-14 10:29:07 +0100

    remove flake8-mecurial (removed upstream)

--- integrationtests/flake8-mercurial/requirements_override.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, python }:
-
-self: super: {
-  "pytest-runner" = super."pytest-runner".overrideDerivation
-    (old: { buildInputs = old.buildInputs ++ [ self."setuptools-scm" ]; });
-
-  "mccabe" = super.mccabe.overrideDerivation
-    (old: { buildInputs = old.buildInputs ++ [ self."pytest-runner" ]; });
-}
